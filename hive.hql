16ba021b9270ad4b9e63dc1f919460cb20796f2d
Helder Rodrigues <helderarrodrigues@gmail.com>
2020-12-04 14:59:30 +0100

    exercice extra 2nd half

--- hive.hql
+++ hive.hql
@@ -110,3 +110,22 @@ where PickupRegionID is not null and PickupRegionID <> ''
 group by concat(PickupRegionID,"_",cast(date_format(tripstarttimestamp, "u") as INT) -1,
     date_format(tripstarttimestamp, "_hh_aaa"));
 
+-- top 3 companies driving from the most popular locations
+with trips_per_company as (
+    select pickupregionid,company, count(*) ntrips
+    from taxi_trips
+    where company is not null and company <> ''
+        and pickupregionid is not null and pickupregionid <> ''
+    group by pickupregionid,company),
+ranked_trips_per_company as (
+    select pickupregionid,company, ntrips,
+           row_number() over (partition by pickupregionid order by ntrips desc) r,
+           sum(ntrips) over (partition by pickupregionid) total_sum
+    from trips_per_company),
+filtered_data as (
+    select pickupregionid,company, total_sum,ntrips,r
+    from ranked_trips_per_company
+    where r <= 3 and total_sum >= 5000
+    order by total_sum desc, r asc)
+select pickupregionid,company,ntrips
+from filtered_data;
--- sparksql.py
+++ sparksql.py
@@ -73,4 +73,26 @@ spark.sql('select concat(PickupRegionID,"_",day_off_week(tripstarttimestamp),dat
           'group by concat(PickupRegionID,"_",day_off_week(tripstarttimestamp),date_format(tripstarttimestamp, "_hh_a"))') \
     .show(truncate=False)
 
+
+
+spark.sql('with trips_per_company as ( '
+          '    select pickupregionid,company, count(*) ntrips '
+          '    from taxi_trips '
+          '    where company is not null and company <> "" '
+          '       and pickupregionid is not null and pickupregionid <> "" '
+          '    group by pickupregionid,company), '
+          'ranked_trips_per_company as ( '
+          '    select pickupregionid,company, ntrips, '
+          '           row_number() over (partition by pickupregionid order by ntrips desc) r, '
+          '           sum(ntrips) over (partition by pickupregionid) total_sum '
+          '    from trips_per_company), '
+          'filtered_data as ( '
+          '    select pickupregionid,company, total_sum,ntrips,r '
+          '    from ranked_trips_per_company '
+          '    where r <= 3 and total_sum >= 5000 '
+          '    order by total_sum desc, r asc) '
+          'select pickupregionid,company,ntrips '
+          'from filtered_data')\
+    .show(truncate=False)
+
 spark.stop()
